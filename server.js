250d3db0fca5b84371400cbcb77991c69255765f
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2025-05-26 23:52:39 +0400

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const {
+ Server
+} = require('socket.io');
 const path = require('path');
 
 const app = express();
@@ -12,44 +14,61 @@ const rooms = {};
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-  socket.on('join-room', roomId => {
-    socket.join(roomId);
-    rooms[socket.id] = roomId;
+ socket.on('join-room', roomId => {
+  socket.join(roomId);
+  rooms[socket.id] = roomId;
 
-    const devices = io.sockets.adapter.rooms.get(roomId)?.size || 0;
-    socket.emit('room-joined', { roomId, devices });
-    io.to(roomId).emit('room-updated', { devices });
+  const devices = io.sockets.adapter.rooms.get(roomId)?.size || 0;
+  socket.emit('room-joined', {
+   roomId, devices
   });
+  io.to(roomId).emit('room-updated', {
+   devices
+  });
+ });
 
-  socket.on('leave-room', roomId => {
-    socket.leave(roomId);
-    delete rooms[socket.id];
-    const devices = io.sockets.adapter.rooms.get(roomId)?.size || 0;
-    io.to(roomId).emit('room-updated', { devices });
+ socket.on('leave-room', roomId => {
+  socket.leave(roomId);
+  delete rooms[socket.id];
+  const devices = io.sockets.adapter.rooms.get(roomId)?.size || 0;
+  io.to(roomId).emit('room-updated', {
+   devices
   });
+ });
 
-  socket.on('send-file-meta', ({ roomId, metadata }) => {
-    socket.to(roomId).emit('file-meta', metadata);
-  });
+ socket.on('send-file-meta', ({
+  roomId, metadata
+ }) => {
+  socket.to(roomId).emit('file-meta', metadata);
+ });
 
-  socket.on('send-file-chunk', ({ roomId, chunk }) => {
-    socket.to(roomId).emit('file-chunk', chunk);
-  });
+ socket.on('send-file-chunk', ({
+  roomId, chunk
+ }) => {
+  socket.to(roomId).emit('file-chunk', chunk);
+ });
 
-  socket.on('send-cancel-transfer', roomId => {
-    socket.to(roomId).emit('cancel-transfer');
+ socket.on("send-cancel-transfer", ({
+  roomId, fileName
+ }) => {
+  socket.to(roomId).emit("send-cancel-transfer", {
+   fileName
   });
+ });
 
-  socket.on('disconnect', () => {
-    const roomId = rooms[socket.id];
-    if (roomId) {
-      delete rooms[socket.id];
-      const devices = io.sockets.adapter.rooms.get(roomId)?.size || 0;
-      io.to(roomId).emit('room-updated', { devices });
-    }
-  });
+
+ socket.on('disconnect', () => {
+  const roomId = rooms[socket.id];
+  if (roomId) {
+   delete rooms[socket.id];
+   const devices = io.sockets.adapter.rooms.get(roomId)?.size || 0;
+   io.to(roomId).emit('room-updated', {
+    devices
+   });
+  }
+ });
 });
 
 server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+ console.log('Server running on http://localhost:3000');
 });
