c4217e91b9ae3547ef9dcbc70ff3cc3b5e63d226
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2025-05-29 18:31:02 +0400

    update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://me-niyas-ali.github.io", // Frontend URL
+    origin: "https://me-niyas-ali.github.io/p2p", // Frontend URL
     methods: ["GET", "POST"]
   }
 });
