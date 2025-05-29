16ce2dbc35aaba67d62a030b033048e4ffd09f79
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2025-05-29 10:54:55 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
+// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: 'https://me-niyas-ali.github.io',
+    methods: ['GET', 'POST']
   }
 });
 
-const peers = new Set();
+const rooms = {};
 
 io.on('connection', (socket) => {
-  socket.on('register-peer', (peerId) => {
-    socket.peerId = peerId;
-    peers.add(peerId);
-    io.emit('peer-list', Array.from(peers));
-  });
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(socket.id);
+    
+    const isHost = rooms[roomId][0] === socket.id;
+    io.to(socket.id).emit('room-joined', { host: isHost, peerId: socket.id });
+    io.to(roomId).emit('peer-count', rooms[roomId].length);
 
-  socket.on('get-peers', () => {
-    io.emit('peer-list', Array.from(peers));
-  });
+    socket.on('leave-room', () => {
+      socket.leave(roomId);
+      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      io.to(roomId).emit('peer-count', rooms[roomId].length);
+    });
 
-  socket.on('disconnect', () => {
-    if (socket.peerId) {
-      peers.delete(socket.peerId);
-      io.emit('peer-list', Array.from(peers));
-    }
+    socket.on('disconnect', () => {
+      if (rooms[roomId]) {
+        rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+        io.to(roomId).emit('peer-count', rooms[roomId].length);
+      }
+    });
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server running...');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
